d36ae4f4baf17d3553ec47cd7259bc56a81ce717
ひろ <koki.hiro0723@gmail.com>
2024-09-13 15:00:54 +0900

    ナビゲーションにメニューを追加

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,10 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { getAuthSession } from "@/lib/nextauth";
 import "./globals.css";
 import Navigation from "@/components/auth/Navigation";
+import AuthProvider from "@/components/providers/AuthProvider";
+import ToastProvider from "@/components/providers/ToastProvider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,39 +22,47 @@ export const metadata: Metadata = {
   description: "Next.js+DRF入門",
 };
 
-export default function RootLayout({
+const RootLayout = async ({
   children,
 }: Readonly<{
   children: React.ReactNode;
-}>) {
+}>) => {
+  // 認証情報取得
+  const user = await getAuthSession();
+
   return (
     <html lang="ja">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="flex min-h-screen flex-col">
-          <Navigation />
+        <AuthProvider>
+          <div className="flex min-h-screen flex-col">
+            <Navigation user={user} />
+            <ToastProvider />
 
-          <main className="container mx-auto max-w-screen-md flex-1 px-2">
-            {children}
-          </main>
+            <main className="container mx-auto max-w-screen-md flex-1 px-2">
+              {children}
+            </main>
 
-          {/* フッター */}
-          <footer className="py-5">
-            <div className="text-center text-sm">
-              Copyright © All rights reserved |{" "}
-              <a
-                href="https://www.youtube.com/@fullstackchannel"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="underline"
-              >
-                FullStackChannel
-              </a>
-            </div>
-          </footer>
-        </div>
+            {/* フッター */}
+            <footer className="py-5">
+              <div className="text-center text-sm">
+                Copyright © All rights reserved |{" "}
+                <a
+                  href="https://www.youtube.com/@fullstackchannel"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="underline"
+                >
+                  FullStackChannel
+                </a>
+              </div>
+            </footer>
+          </div>
+        </AuthProvider>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
